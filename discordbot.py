c05411dacdc11c20e3109570b7364087e9fd68a5
seaurchin922 <74389431+seaurchin922@users.noreply.github.com>
2020-12-28 07:44:23 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -93,7 +93,6 @@ def maxDgtOfList(l):
 
 @tasks.loop(seconds=60)
 async def update_border(): # 自動ボーダー送信機能
-    
     shiuntench = client.get_channel(776432703964708865) 
     bdrbotch = client.get_channel(786021632522846269) # ボーダーbotチャンネルのID
     botlogch = client.get_channel(790298428957261855)
@@ -104,190 +103,6 @@ async def update_border(): # 自動ボーダー送信機能
     # イベ開始一時間後から30分起きの時刻を設定する
     await botlogch.send('looping on ' + nowTime.strftime('%m/%d %H:%M:%S')) # ループ中であることを端末に出力
 
-
-
-    ###   現在開催中のイベント情報の取得   ###
-    nowEventData_url = 'https://api.matsurihi.me/mltd/v1/events/?at=' + str(nowTime)
-
-    response = requests.get(nowEventData_url)
-    nowEventData = response.json()
-
-    try:
-        evId = str(nowEventData[0]['id'])
-    except IndexError:
-        await botlogch.send('イベント未開催') # 安定したらコメントアウト
-        return # イベント未開催
-
-    evType = nowEventData[0]['type']
-    evName = nowEventData[0]['name']
-
-    beginTime = datetime.datetime.strptime(nowEventData[0]['schedule']['beginDate'],'%Y-%m-%dT%H:%M:%S+09:00') # イベント開始日時
-    endTime = datetime.datetime.strptime(nowEventData[0]['schedule']['endDate'],'%Y-%m-%dT%H:%M:%S+09:00') # イベント終了日時
-
-    eventTime = endTime + datetime.timedelta(seconds=1) - beginTime # 開催期間 = 終了時間 - 開始時間
-
-    # !!!debug!!!
-    #debug = int(48*(nowTime - beginTime).days + (nowTime - beginTime).seconds/1800 - 1)
-    #updateMissTimes = 2 
-
-
-
-    ###   イベント形式の判別   ###
-    if evType not in evType_to_Colour:
-        await botlogch.send('開催中のイベントはランキング形式ではありません') # 安定したらコメントアウト
-        return # ランキング形式イベントではない
-    
-    boostTime = datetime.datetime.strptime(nowEventData[0]['schedule']['boostBeginDate'],'%Y-%m-%dT%H:%M:%S+09:00') # 折り返し日時
-
-
-
-    ###   現在のスコアデータと最終更新時刻(nowBdrData)の取得   ###
-    printRanks = '/1-20' # 出力する順位(1~20位から変更可)
-    nowBdrData_url = 'https://api.matsurihi.me/mltd/v1/events/' + evId + '/rankings/logs/eventPoint' + printRanks
-
-    response = requests.get(nowBdrData_url)
-    nowBdrData = response.json()
-    
-    try:
-        currentSummaryTime = datetime.datetime.strptime(nowBdrData[0]['data'][-1]['summaryTime'],'%Y-%m-%dT%H:%M:%S+09:00')
-        # !!!debug!!!
-        #datetime.datetime.strptime(nowBdrData[0]['data'][debug]['summaryTime'],'%Y-%m-%dT%H:%M:%S+09:00')
-        # !!!debug!!!
-    except IndexError:
-        await botlogch.send('イベント開始直後です')
-        return # 開催されてから更新されていない
-
-    elapsedTime = currentSummaryTime - beginTime # 経過時間 = 現在時刻 - 開始時間
-    remainingTime = endTime - currentSummaryTime # 残り時間 = 終了時間 - 現在時刻
-
-    
-
-
-    ###   ひとつ前のスコアデータと最終更新時刻(beforeBdrData)の取得   ###
-    try:
-        beforeBdrData = json.load(open('currentScores/' + evId + '.json', 'r'))
-        # !!! ↑ debug ↓ !!!
-        #beforeBdrData = json.load(open('currentScores/test/' + evId + '.json', 'r'))
-        
-        beforeSummaryTime = datetime.datetime.strptime(beforeBdrData[0]['data'][-1]['summaryTime'],'%Y-%m-%dT%H:%M:%S+09:00')
-        # !!! ↑ debug ↓ !!!
-        #beforeSummaryTime = datetime.datetime.strptime(beforeBdrData[0]['data'][debug-1-updateMissTimes]['summaryTime'],'%Y-%m-%dT%H:%M:%S+09:00')
-    except OSError:
-        # currentScoresファイル無い(=初更新)ので"evId + '.json'" ファイルを作る
-        Path('currentScores/' + evId + '.json').touch()
-        # !!! ↑ debug ↓ !!!
-        #Path('currentScores/test/' + evId + '.json').touch()
-        with open('currentScores/' + evId + '.json', 'w') as f:
-        # !!! ↑ debug ↓ !!!
-        #with open('currentScores/test/' + evId + '.json', 'w') as f:
-            json.dump([], f)
-        beforeBdrData = json.load(open('currentScores/' + evId + '.json', 'r'))
-        # !!! ↑ debug ↓ !!!
-        #beforeBdrData = json.load(open('currentScores/test/' + evId + '.json', 'r'))
-    
-    # 最終更新時刻とひとつ前の最終更新時刻の表示
-    #await botlogch.send('currentSummaryTime = {}\nbeforeSummaryTime = {}'.format(currentSummaryTime, beforeSummaryTime)) #安定したら消す
-
-
-
-    ###   ボーダー情報が更新されたか(=beforeSummaryTime < currentSummaryTime)判定   ###
-    updateMiss = False # 集計漏れ
-    updateInterval = (currentSummaryTime - beforeSummaryTime).seconds / 1800 # 前データの最終更新時刻と取得した最終更新時刻に更新間隔(=30分)何回分の差があるか
-    if updateInterval == 0.0: # 前データの最終更新時刻と取得した最終更新時刻が一致(=まだ更新無し) 
-        await botlogch.send('ボーダー情報はまだ更新されていません') # 安定したらコメントアウト
-        return
-        # !!! ↑ debug時コメントアウト !!!
-        pass
-    elif updateInterval > 1.0:  # 前回の集計から30分以上経過している
-        await botlogch.send('集計漏れ有')
-        updateMiss = True
-    elif updateInterval == 1.0: # 前回の集計から30分ちょうど経過している
-        await botlogch.send('ボーダー情報が更新されました')
-    else: # updateIntervalが(0.0, 1.0)内もしくは負の数，本来あり得ない
-        await botlogch.send('取得データに誤りがあります')
-        return
-
-
-
-    ###   ひとつ前と現在のイベントのボーダースコアのリスト化   ###
-    beforeScores = [] # 前回のスコアのリスト
-    bdrRanks = [] # 各ボーダーの順位（基本的に不変）
-    nowScores = [] # 現時点でのスコアのリスト
-
-    # リストへの書き込み
-    for b, n in zip(beforeBdrData, nowBdrData):
-        try:
-            beforeScores.append(int(b['data'][-1]['score']))
-            # #!!! ↑ debug ↓ !!!
-            #beforeScores.append(int(b['data'][debug-1-updateMissTimes]['score']))
-        except IndexError:
-            beforeScores.append(0)
-        bdrRanks.append(int(n['rank']))
-        nowScores.append(int(n['data'][-1]['score']))
-        # !!! ↑ debug ↓ !!!
-        #nowScores.append(int(n['data'][debug]['score']))
-
-    # currentScoresファイルの更新
-    with open('currentScores/' + evId + '.json', 'w') as f:
-    # !!! ↑ debug ↓ !!!
-    #with open('currentScores/test/' + evId + '.json', 'w') as f:
-        json.dump(nowBdrData, f, indent=2)
-
-
-
-    ###   titleにタイトルを代入   ###
-    title = '' # タイトル用の空のstr変数
-    msg = '' # メッセージ用の空のstr変数
-
-    title += evName # 開催中のイベントの名前をtitleに追加
-
-    msg += '開催期間：{} ～ {} ({}h)\n'.format(beginTime.strftime('%m/%d %H:%M'), endTime.strftime('%m/%d %H:%M'), int(eventTime.total_seconds() / 3600)) # 開催期間をmsgに追加
-    msg += '折り返し：{}\n'.format(boostTime.strftime('%m/%d %H:%M')) # 折り返し日時をmsgに追加
-    msg += '\n'
-    msg += currentSummaryTime.strftime('%m/%d %H:%M') + ' 更新 ' # 更新時間をmsgに追加
-
-    elapsedTimeRaito = elapsedTime.total_seconds()/eventTime.total_seconds() # 経過時間の割合
-    remainingTime_hours =  int(remainingTime.days * 24 + (remainingTime.seconds + 1) / 3600)
-    remainingTime_minutes = int((remainingTime.seconds + 1) / 60 + remainingTime.days * 1440 - remainingTime_hours * 60)
-    msg += '({:.1%}経過 残り{}時間{}分)'.format(elapsedTimeRaito,remainingTime_hours,remainingTime_minutes)
-    
-
-
-    ###   msgにボーダー表を代入   ###
-    if updateMiss: # もし集計漏れがある場合msgに太文字で追加
-        msg += '**！集計漏れあり！**\n'
-
-    if not updateMiss: # 集計漏れ無いので30分速
-        msg += '``` #:     score    -0.5h\n-----------------------\n'
-    else: # 集計漏れあるため -0.5h を -(updateInterval / 2)h とする
-        msg += '``` #:     score    -{}h\n-----------------------\n'.format(updateInterval / 2)
-
-    for r, n, b in zip(bdrRanks, nowScores, beforeScores):
-        diff = n - b
-        msg += '{:>2,d}: {:>9,d} (+{:>6,d}'.format(r,n,diff)
-        # "{:>2,d}" の2は表示する順位の最大桁数に合わせて変更 (100,000位まで表示するのならば "{:>6,d}" )
-        if evType == 3: # シアターならdiffから貯めか消費か判断、プレイ回数を出力
-            if diff % 595 == 0 and diff != 0: # 貯め
-                playtimes = int(diff/595)
-                msg += ' =    SV * {:>2}'.format(playtimes)
-            elif diff % 2148 == 0 and diff != 0: # 消費
-                playtimes = int(diff/2148)
-                msg += ' = EVENT * {:>2}'.format(playtimes)
-        msg += ')\n'
-    msg += '```\n配信元：[matsurihi.me](https://www.matsurihi.me/)'
-
-
-
-    ###   書式を整えてメッセージを送信   ###
-    # Fantasiaへのリンク
-    fantasia_url = 'https://mltd.matsurihi.me/events/' + evId
-    # サムネイルのリンク
-    image_url = 'https://storage.matsurihi.me/mltd/event_bg/{:0>4}.png'.format(int(evId))
-
-    embed = discord.Embed(title=title, url=fantasia_url, description=msg, color=evType_to_Colour[evType])
-    embed.set_thumbnail(url=image_url)
-    #await shiuntench.send(embed=embed)
-    await bdrbotch.send(embed=embed)
 
 @client.event
 async def on_ready():
@@ -401,8 +216,8 @@ async def on_message(message):
                         printRanks = '/rankings/logs/idolPoint/' + str(idolId) + '/1-3,10,100,1000'
                 title += ' {} ランキング'.format(idolName)
             else:
-                try:
-                    printRanks = '/rankings/logs/eventPoint/' + str(message.content.split()[2])
+				try:
+					printRanks = '/rankings/logs/eventPoint/' + str(message.content.split()[2])
                 except IndexError:
                     printRanks = '/rankings/logs/eventPoint/1-3,100,2500,5000,10000,25000,100000'           
             
