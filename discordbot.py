99389d6549accea9e7f3ba85944770c148e256c4
seaurchin922 <74389431+seaurchin922@users.noreply.github.com>
2020-12-28 07:03:11 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,21 +1,32 @@
+import discord
+import json
+import requests
+import datetime
+from discord.ext import tasks
+from pathlib import Path
 from discord.ext import commands
 import os
 import traceback
 
-bot = commands.Bot(command_prefix='/')
+client = discord.Client()
+
 token = os.environ['DISCORD_BOT_TOKEN']
 
+@client.event
+async def on_ready():
+    # 状態を '☆ピコピコプラネット☆を視聴中' にする
+    activity = discord.Activity(name='☆ピコピコプラネット☆', type=discord.ActivityType.watching)
+    await client.change_presence(activity=activity)
+    #update_border.start()
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
+@client.event
+async def on_message(message):
+    # メッセージ送信者がBotだった場合は無視する
+    if message.author.bot:
+        return
+    # 「/neko」と発言したら「にゃーん」が返る処理
+    if message.content == '/neko':
+        await message.channel.send('にゃーん')
 
 
-@bot.command()
-async def pang(ctx):
-    await ctx.send('peng')
-
-
-bot.run(token)
+client.run(token)
